8c90379750b3eb2b12c7a5e28ee7932a2e189bdb
Raphael Almeida <raphaels10@hotmail.com>
2020-11-06 04:21:35 -0300

    adicionada funcionalidade de scroll infinito ao chat
    ainda precisa ser revista

--- frontend/src/user/privatemessage.jsx
+++ frontend/src/user/privatemessage.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import InfiniteScroll from 'react-infinite-scroll-component'
 import axios from 'axios'
 import { connect } from 'react-redux'
 import './privatemessage.css'
@@ -9,21 +10,42 @@ function PrivateMessage(params) {
 
     const [messageList, setMessageList] = useState([])
     const [messageText, setMessageText] = useState("")
+    const [lastTen, setLastTen] = useState([])
+    const [hasInited, setHasInited] = useState(false)
+    const [currentPage, setCurrentPage] = useState(2)
+    const [hasMore, setHasMore] = useState(true)
+
 
     const { token } = JSON.parse(localStorage.getItem("user-session")) || ''
     parseMessageDate("2020-11-04T04:51:01.094Z")
 
     const { message_id } = params
+
     useEffect(() => {
-        const interval_message = setInterval(getMessages, 1500)
-        getMessages()
+        const interval_message = setInterval(getLastTen,3000)
+        getLastTen("init")
         return () => clearInterval(interval_message)
     }, [])
 
-    function getMessages() {
-        axios.get(`${BASE_URL}/conversationmessage/${message_id}`)
+    function getLastTen(str) {
+        const _str = str || ""
+        axios.get(`${BASE_URL}/conversationmessage/${message_id}?page=1`)
             .then(r => {
-                setMessageList(r.data.messages)
+                if(_str==="init") {
+                    setMessageList(r.data)
+                    return
+                }
+                setMessageList(m => {
+                    const messageIds = m.map(m => m._id)
+                    const responseIds = r.data.map(r => r._id)
+                    const number_of_new_messages = responseIds.indexOf(messageIds[0])
+
+                    const sliced_messages = m.slice(10-number_of_new_messages)
+                    sliced_messages.unshift(...r.data)
+                    return sliced_messages
+                })
+
+                
             })
     }
     function postMessage(e) {
@@ -40,16 +62,41 @@ function PrivateMessage(params) {
         return UTCDate.toLocaleString("en-gb")
     }
 
+    function handleNext() {
+
+        axios.get(`${BASE_URL}/conversationmessage/${message_id}?page=${currentPage}`)
+        .then(r => {
+            if(r.data.length === 0) {
+                setHasMore(false)
+                return
+            }
+
+            setMessageList([...messageList, ...r.data,])
+            setCurrentPage(currentPage + 1)
+        })
+
+    }
+    
+
     return (
         <>
             <h2>Conversa com {params.destinatary}</h2>
-            <div className="private-message-container">
-                {messageList.map(message => (
-                    <div className={`single-message ${params.username === message.from ? "message-from-you": "message-from-other"}`} key={message._id}>
-                        <p>{message.text}<span className="time"> {parseMessageDate(message.createdAt)}</span></p>
-                    </div>
-                    
-                ))}
+            <div id="scrollable-div" className="private-message-container">
+
+                
+                <InfiniteScroll dataLength={messageList.length} loader={<h4>Loading...</h4>}
+                hasMore={hasMore} scrollableTarget="scrollable-div" next={handleNext}
+                style={{ overflow: "visible" , display: 'flex', flexDirection: 'column-reverse' }}
+                inverse={true}>
+
+                    {messageList.map(message => (
+                        <div className={`single-message ${params.username === message.from ? "message-from-you": "message-from-other"}`} key={`${message._id}b`}>
+                            <p>{message.text}<span className="time"> {parseMessageDate(message.createdAt)}</span></p>
+                        </div>
+                        
+                    ))}
+
+                </InfiniteScroll>
 
             </div>
             <hr />
