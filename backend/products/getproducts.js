4dc75f39d00f8c1929d510fd5cf45694d5e9d0eb
Raphael Almeida <raphaels10@hotmail.com>
2020-11-18 01:35:22 -0300

    alterada a rota getproduct

--- backend/products/getproducts.js
+++ backend/products/getproducts.js
@@ -1,8 +1,29 @@
 const Product = require('../database/products')
 
 module.exports = (req, res, next) => {
+    const query = req.query
     Product.find((err, products) => {
         if (err) return res.status(400).json({error: ["Não foi possível resgatar os produtos"]})
+        if (Object.keys(query).length > 0) {
+            var filtered = []
+            for (key in query) {
+                if (key === "name") {
+                    console.log("entrou no if")
+                    const nextFiltered = products.filter(p =>
+                         p.name.toLowerCase().includes(query.name.toLowerCase()) || p.game.toLowerCase().includes(query.name.toLowerCase())
+                         )
+                    console.log(nextFiltered)
+                    filtered = filtered.concat(nextFiltered)
+                    console.log(filtered)
+                    continue
+                }
+                const nextFiltered = products.filter(p => p[key].toLowerCase() === query[key].toLowerCase())
+                filtered = filtered.concat(nextFiltered)
+            }
+
+            return res.json(filtered)
+
+        }
         return res.json(products)
     })
 }
